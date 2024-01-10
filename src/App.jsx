624e8b46124dfc2ea94759d1b4fa8a96410a7450
amin-da <darabniya@yahoo.com>
2024-01-10 11:35:23 +0330

    initial commit - create & project with vite and add tailwind css on project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="text-2xl text-primary max-w-7xl mx-auto ">
+      <h2 className="text-success">Budget Manager</h2>
+    </div>
+  );
+}
+
+export default App;
